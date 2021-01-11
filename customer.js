5204b06ce59c8e1625d1f48829050cbd28eea478
Sunday Abam <abamsunday5@gmail.comgit config --edit --globalcodemkdird>
2021-01-12 00:32:20 +0100

    first

---
+++ customer.js
@@ -0,0 +1,10 @@
+let time = 0;
+let timer = setInterval(function ()
+{
+    time = + 2;
+    console.log(time + 'Seconds have passed');
+    if (time > 5)
+    {
+        clearInterval(timer);
+    }
+}, 3000);
