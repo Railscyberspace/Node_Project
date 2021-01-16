9be71e6b896f6d08c409214fb2c239bbfe2d0049
Sunday Abam <abamsunday5@gmail.comgit config --edit --globalcodemkdird>
2021-01-16 19:58:48 +0100

    first

--- url.js
+++ url.js
@@ -8,13 +8,19 @@ const server = http.createServer(function (req, res)
     {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         fs.createReadStream(__dirname + '/index.html').pipe(res);
-    } else (req.url === '/contact')
+    } else if (req.url === '/contact')
     {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    } else if (req.url === './api/Ruby')
+    {
+        const Ruby = [{ name: 'Jason', age: 5 }, { name: 'Zenitha', age: 11 }];
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(Ruby));
     }
 });
 
 
 server.listen(3000, '127.0.0.1');
 console.log('Listening at port .....3000');
+
